c6558496ebd9e14166de0ae4c2d52d4f34122214
Alex Mouton <thomas.mouton@gmail.com>
2018-12-06 21:00:05 -0800

    repeat

--- src/Main.purs
+++ src/Main.purs
@@ -1,18 +1,79 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
-import Effect.Console (log)
+
+import Control.Lazy (fix, defer)
+
 import Data.String
+import Data.Maybe
+import Data.Either
 import Data.Int.Parse
 import Data.Foldable
 import Data.Array
 import Data.Function
+import Data.Map as M
+import Data.Newtype
+import Data.Traversable (scanl, class Traversable)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Data.List.Lazy (List, cycle, fromFoldable)
+import Data.List.Lazy.Types (Step(..), step) as Lt
+
+import Effect (Effect)
+import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  log $ show $ foldl (+) 0 $ catMaybes $ (flip parseInt) (toRadix 10) <$> split (Pattern "\n") values
+  log $ show $ calibration
+  log $ show $ firstCross
 
+prepared :: Array Int
+prepared = catMaybes $ toInt <$> toValues values
+
+toValues :: String -> Array String
+toValues = split (Pattern "\n")
+
+toInt :: String -> Maybe Int
+toInt = (flip parseInt) (toRadix 10)
+
+calibration :: Int
+calibration = sum prepared
+
+sum :: ∀ f. Foldable f => f Int -> Int
+sum = foldl (+) 0
+
+---
+type Cross = Either Int { seen :: M.Map Int Boolean }
+
+firstCross :: Cross
+firstCross = repeat $ cycle $ defer (\_ -> fromFoldable prepared)
+
+repeat :: List Int -> Cross
+repeat l = _.cross $ foldc { current: 0, cross: cempty, list: l }
+
+cempty :: Cross
+cempty = Right { seen: M.insert 0 true M.empty }
+
+consider :: Cross -> Int -> Cross
+consider (Right r) i | (M.member i r.seen) = Left i
+consider (Right r) i = Right { seen: (M.insert i true r.seen) }
+consider a _ = a
+
+type FoldCross = { current :: Int, cross :: Cross, list :: List Int }
+
+foldc :: FoldCross -> FoldCross
+foldc fc =
+  case fc.cross of
+    Left a -> fc
+    Right b ->
+      case Lt.step fc.list of
+        Lt.Nil -> fc
+        Lt.Cons a as ->
+          let cur = fc.current + a
+          in
+            foldc $ { current: cur, cross: (consider fc.cross cur), list: as }
+
+values :: String
 values = """+13
 -18
 +13
--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,26 @@
 module Test.Main where
 
 import Prelude
-import Effect (Effect)
+
 import Effect.Console (log)
+import Effect.Class
+import Data.Time.Duration (Milliseconds(..))
+import Effect (Effect)
+import Effect.Aff (launchAff_, delay)
+import Test.Spec (pending, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (run)
+
+import Main
 
 main :: Effect Unit
-main = do
-  log "You should add some tests."
+main = run [consoleReporter] do
+  describe "purescript-spec" do
+    describe "consider" do
+      it "miss" do
+        liftEffect $ log $ show (consider empty 1)
+      it "miss" do
+        liftEffect $ log $ show (consider (consider empty 2) 1)
+      it "hit" do
+        liftEffect $ log $ show (consider (consider (consider empty 1) 2) 1)
